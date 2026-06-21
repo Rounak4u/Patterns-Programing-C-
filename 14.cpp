e45e54461a91f205f8e02770b20eedaff839036d
Rounak Kumar <153853069+Rounak4u@users.noreply.github.com>
2026-06-22 03:16:06 +0530

    Add files via upload

---
+++ 14.cpp
@@ -0,0 +1,22 @@
+
+#include<iostream>
+using  namespace std;
+
+int main()
+{
+    int n=5;
+    int x;
+
+    for(int i=1;i<=n;i++)
+    {
+        x=n-1+1;
+
+        for(int j=1;j<=n;j++)
+        {
+            cout<<x<<"";
+            x=x+n;
+
+        }
+        cout<<endl;
+    }
+}
